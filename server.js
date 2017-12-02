ff09b4df0574ee8905bc45eae83cc4f6a8d46399
Jim Pai <chunan.pai@gmail.com>
2017-12-02 15:34:46 -0800

    Modified server.js

--- server.js
+++ server.js
@@ -1,29 +1,53 @@
 const fetch = require('node-fetch');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const heroku = require('./heroku.js');
+const omdb = require('./omdb.js');
 const API_KEY = '852159f0';
 const PORT = 8000;
 
+const jsonParser = bodyParser.json();
+
+let reviews = {};
+let movies = {};
+const token = '';
+
+//app.use('', (req, res) => {
+//    
+//})
+
 app.get('/movie/search/:prefix', (req, res) => {
-    const url = `http://www.omdbapi.com/?apikey=${API_KEY}&s=${req.params.prefix}`;
-    fetch(url)
-    .then(r => r.ok ? r.json() : r.json().then(j => Promise.reject(j)))
-    .then(j => {
-        res.write(JSON.stringify(j));
+    omdb.getMovies(API_KEY, req.params.prefix)
+    .then(searchResult => {
+        res.write(JSON.stringify(searchResult));
         res.end();
     })
     .catch(e => console.warn(e));
 });
 
 app.get('/movie/:title', (req, res) => {
-    const url = `http://www.omdbapi.com/?apikey=${API_KEY}&t=${req.params.title}`;
-    fetch(url)
-    .then(r => r.ok ? r.json() : r.json().then(j => Promise.reject(j)))
-    .then(j => {
-        res.write(JSON.stringify(j));
+    omdb.getMovieInfo(API_KEY, req.params.title)
+    .then(movieInfo => {
+        res.write(JSON.stringify(movieInfo));
         res.end();
     })
     .catch(e => console.warn(e));
+});
+
+app.post('/review/:username', jsonParser, (req, res) => {
+    const body = req.body;
+    console.log(body);
+    const username = body.username;
+    const reviewList = reviews.username;
+    reviewList.append(body.review);
+});
+
+app.delete('/review/:username', jsonParser, (req, res) => {
+    const body = req.body;
+    console.log(body);
+    const username = body.username;
+    
 });
 
 app.listen(PORT, () => {
